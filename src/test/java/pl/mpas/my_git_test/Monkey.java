95f046b190268bcfb741bd99051f039b93895e75
Asia <kurowska.asia@gmail.com>
2018-12-01 15:04:46 +0100

    [add] - animal Monkey

--- src/test/java/pl/mpas/my_git_test/Monkey.java
+++ src/test/java/pl/mpas/my_git_test/Monkey.java
@@ -1,4 +1,13 @@
 package pl.mpas.my_git_test;
 
 public class Monkey extends Animal {
+    public Monkey() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Monkey{}"+
+                "dog"+ "dsad";
+    }
 }
