8a7846a691596996c4975ebe13a687d1f6d0ed6c
Asia <kurowska.asia@gmail.com>
2018-12-01 11:49:50 +0100

    [change] - from Car to Auto

--- src/test/java/pl/mpas/my_git_test/Auto.java
+++ src/test/java/pl/mpas/my_git_test/Auto.java
@@ -1,6 +1,6 @@
 package pl.mpas.my_git_test;
 
-public class Car {
+public class Auto {
     private String brand;
     private String model;
 
@@ -12,7 +12,7 @@ public class Car {
         return model;
     }
 
-    public Car(String brand, String model){
+    public Auto(String brand, String model){
         this.brand=brand;
         this.model=model;
     }
