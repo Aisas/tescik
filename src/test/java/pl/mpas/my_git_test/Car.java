39b322d50a7f2a1c338e905cbc1fc8d301a5d883
Asia <kurowska.asia@gmail.com>
2018-12-01 11:43:20 +0100

    [add] - Car marka model gettery settery

--- src/test/java/pl/mpas/my_git_test/Car.java
+++ src/test/java/pl/mpas/my_git_test/Car.java
@@ -3,4 +3,17 @@ package pl.mpas.my_git_test;
 public class Car {
     private String brand;
     private String model;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Car(String brand, String model){
+        this.brand=brand;
+        this.model=model;
+    }
 }
